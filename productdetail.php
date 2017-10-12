46d79166a66da5f2b523a30f3bb9df03f0d00361
victoria echevarria <viclee953@gmail.com>
2017-10-12 18:50:31 -0400

    more stuff

--- productdetail.php
+++ productdetail.php
@@ -1,9 +1,9 @@
 <?php
-$numOfItems = !empty($_GET['numOfItems']) ? $_GET['numOfItems'] : '';
+// $numOfItems = !empty($_GET['numOfItems']) ? $_GET['numOfItems'] : '';
 $product_id = !empty($_GET['product_id']) ? $_GET['product_id'] : '';
 
 session_start();
-$_SESSION["cartItems"] = $_SESSION["cartItems"] +  $numOfItems;
+$_SESSION["cartItems"] = $_SESSION["cartItems"] +  $_GET['numOfItems'];
 $pageTitle = "Product Details";
 include("inc/nav.php");
 ?>
